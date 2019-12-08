a0df90c7e2502678e125907135df5b039fdf5d80
Carlos Martinez <edu6jas@gmail.com>
2019-12-07 23:18:51 -0430

    ignoring app folder, switch to frontend folder

--- app/serverAPI/models/noticia.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Noticia{
-    constructor({titulo, descripcion, fecha, imagen}){
-        this.titulo = titulo;
-        this.descripcion =  descripcion;
-        this.fecha = fecha;
-        this.imagen = imagen;
-    }
-}
--- app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { hydrate } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-// components
-import AppDOM from './app/app';
-// style
-import '../public/bulma.css';
-// render in browser
-hydrate(<AppDOM />, document.getElementById("root") );
