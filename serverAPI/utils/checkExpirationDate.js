5274ee6ac3f304e520d10eaa5bcd162e05975b11
Carlos Martinez <edu6jas@gmail.com>
2021-01-29 23:33:31 -0400

    set date to default again, in codeIsExpired()

--- serverAPI/utils/checkExpirationDate.js
+++ serverAPI/utils/checkExpirationDate.js
@@ -1,12 +1,9 @@
 // check expiration date, return true if the code is expired
 function codeIsExpired(codeDateExpirationString){
-    console.log(`#####  the codeDateExpirationString: ${codeDateExpirationString} #####`)
+    // console.log(`#####  the codeDateExpirationString: ${codeDateExpirationString} #####`)
     // check date
-    // time in Venezuela
-    const today = new Date( new Date().toLocaleString("es-VE", {timeZone: "America/Caracas"}) );
+    const today = new Date();
     const codeExpirationDate = new Date(codeDateExpirationString);
-    console.log(`esta fecha: ${today} es menor a ${codeExpirationDate} ?????`)
-    console.log(`es menor???: ${today < codeExpirationDate}`)
     // today is still valid
     if(today < codeExpirationDate ) return false; 
     // code is expired
